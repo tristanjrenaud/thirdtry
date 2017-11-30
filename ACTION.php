ae04e4fc5443bcaa37eca30d6c734b37b45d98e1
GUAN4280 <34089228+GUAN4280@users.noreply.github.com>
2017-11-30 14:36:42 -0500

    Add files via upload

---
+++ ACTION.php
@@ -0,0 +1,14 @@
+<body>
+<h1>Your Questions or Comment has been received.</h1>
+Thank you for your Questions and Comment. We will send you the feedback as soon as possible.
+<form action="C:\Users\dell\Desktop\CP 202\final\half-page-carousel\half-page-carousel\index2-discussion.html">
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<button>back</button>
+</form>
+
+</body>
